8ae6fa6bbceb3d4ce32ee78f0bcbbc2377a916da
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2025-12-13 20:59:31 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
-export const App = () => {
+ const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Navbar/>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} /> 
-        <Route path='/collection' element={<Collection/>} /> 
-        <Route path='/about' element={<About/>} /> 
-        <Route path='/contact' element={<Contact/>} /> 
-        <Route path='/product/:productId' element={<Product/>} /> 
-        <Route path='/cart' element={<Cart/>} /> 
-        <Route path='/login' element={<Login/>} /> 
-        <Route path='/placeorder' element={<PlaceOrder/>} /> 
-        <Route path='/orders' element={<Orders/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+/*1.28.35 de kaldım  https://youtu.be/7E6um7NGmeE?list=PLjwm_8O3suyOFd8LTFqgw9v7MqPNtgINA */ 
